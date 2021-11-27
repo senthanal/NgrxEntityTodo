145fd1e9f8aeee083f9dd99aabfd8320e3b31e6d
Senthanal Sirpi Manohar <senthanal@gmail.com>
2021-11-27 18:54:48 +0100

    Added support for storeing state into the local storage using the third party library.

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,9 +1,16 @@
 import { routerReducer } from '@ngrx/router-store';
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 export interface AppState {}
 
 export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer,
 };
 
-export const metaReducers: MetaReducer<AppState>[] = [];
+export function localStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({ keys: ['todos'], rehydrate: true })(reducer);
+}
+
+export const metaReducers: MetaReducer<AppState>[] = [localStorageSyncReducer];
